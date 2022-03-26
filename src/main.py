9e140bc36ff2d337346f5ac37ce04087a8dd08e2
José Ramírez <jseh125@gmail.com>
2022-03-26 11:15:42 -0600

    Agrega ejemplo de con clases

--- src/main.py
+++ src/main.py
@@ -1,2 +1,31 @@
-print('jose manuel')
-print('todo bien')
+class Animal:
+    
+    def __init__(self):
+        print('ok')
+
+    def comer(self): 
+        print('animal comiendo...')
+
+class Oso:
+
+    def comer(self): 
+        print('comiendo...')
+
+class OsoPolar(Oso, Animal):
+
+    def comer(self): 
+        print('polar comiendo...')
+        super().comer()
+        Animal.comer(self)
+
+
+def mostrarDatos(): 
+    n = 234
+    s = "asdf"
+    print('-.....--------')
+
+
+if __name__ == "__main__":
+    n = OsoPolar()
+    n.comer()
+    mostrarDatos()
